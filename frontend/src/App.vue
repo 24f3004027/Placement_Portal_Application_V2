88485efcaf9278c84d3d09ffceb87ac091c31a1f
RSNPIIT <ramrupsatpati@gmail.com>
2026-09-14 09:49:23 +0530

    Updated the main Frontend App herein

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,41 @@
 <template>
-  <router-view />
+  <div class="app-layout">
+    <Navbar />
+    <main class="main-content">
+      <router-view />
+    </main>
+    <Footer />
+  </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style>
+.app-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #0b0f19;
+  color: #f1f5f9;
+}
+
+.main-content {
+  flex: 1;
+  padding: 2rem;
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+</style>
